28ba8cacbff59f359813db4245001628d9e83fd9
maxfedorov <mfglad@gmail.com>
2025-01-17 00:56:06 +0100

    Complete task 2

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import {createRoot} from 'react-dom/client'
+import {restaurants} from "./mock.js";
+
+createRoot(document.getElementById('root')).render(
+    <div>
+        {
+            restaurants.map(restaurant =>
+                <div>
+                    <h2>{restaurant.name}</h2>
+                    <h3>Меню</h3>
+                    <ul>{restaurant.menu.map((item) => (
+                        <li>{item.name}</li>
+                    ))}
+                    </ul>
+                    <h3>Отзывы</h3>
+                    <ul>
+                        {restaurant.reviews.map((review) => (
+                            <li>{review.text}</li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
+    </div>
+)
