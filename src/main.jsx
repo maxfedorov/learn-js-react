173c2bd6290b5e81c899499fcb7c56bd3d982996
maxfedorov <mfglad@gmail.com>
2025-01-17 12:18:28 +0100

    add keys

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import { restaurants } from "./mock.js";
 createRoot(document.getElementById("root")).render(
   <div>
     {restaurants.map((restaurant) => (
-      <div>
+      <div key={restaurant.id}>
         <h2>{restaurant.name}</h2>
         <h3>Меню</h3>
         <ul>
           {restaurant.menu.map((item) => (
-            <li>{item.name}</li>
+            <li key={item.id}>{item.name}</li>
           ))}
         </ul>
         <h3>Отзывы</h3>
         <ul>
           {restaurant.reviews.map((review) => (
-            <li>{review.text}</li>
+            <li key={review.id}>{review.text}</li>
           ))}
         </ul>
       </div>
