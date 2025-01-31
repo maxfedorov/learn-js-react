3c4c6592c1c5bd79a46f8870010b3ea7055d40de
maxfedorov <mfglad@gmail.com>
2025-01-31 15:32:22 +0100

    task 5 completed

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -3,13 +3,9 @@ import styles from "./counter.module.css";
 const Counter = ({ value = 0, onDecrement, onIncrement }) => {
   return (
     <div className={styles.counter}>
-      <button onClick={onDecrement} className={styles.counterButton}>
-        -
-      </button>
+      <button onClick={onDecrement}>-</button>
       {value}
-      <button onClick={onIncrement} className={styles.counterButton}>
-        +
-      </button>
+      <button onClick={onIncrement}>+</button>
     </div>
   );
 };
