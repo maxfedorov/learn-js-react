a993d0d7b7c4a8a189fe04398f77dd772f7e1d53
maxfedorov <mfglad@gmail.com>
2025-02-26 14:33:12 +0100

    move to next

---
+++ src/app/[[...slug]]/page.js
@@ -0,0 +1,11 @@
+import { AppClient } from "./client.js";
+
+// export const generateStaticParams = () => {
+//   return [{ slug: [""] }];
+// };
+
+const Page = () => {
+  return <AppClient />;
+};
+
+export default Page;
--- src/main.jsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./components/app/App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
